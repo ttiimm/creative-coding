269c765bfcdc13a9cf0d29e5d17584c629147767
Tim Likarish <likarish@gmail.com>
2024-06-17 10:29:10 -0400

    [week 4] Tim & Nick do some flag art

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,46 @@
 import * as p5 from 'p5';
 
 export const sketch = (p: p5) => {
+    let font: p5.Font;
+    let counter = 0;
+    let planetX = 50;
+    let planetY = 50;
+    
+    p.preload = () => {
+        font = p.loadFont('IosevkaTerm-Medium.ttf');
+    }
+
     p.setup = () => {
-        p.createCanvas(400, 400);
+        p.createCanvas(640, 480);
+        // Set text characteristics
+        p.textFont(font);
+        p.textSize(32);
+        p.textAlign(p.CENTER, p.CENTER);
+        p.rectMode(p.CENTER);    
     }
 
     p.draw = () => {
-        p.background(220);
-        p.ellipse(50,50,80,80);
+        p.background(0);
+        p.fill(76, 187, 23);
+        p.rect(p.width / 2, p.height / 2, 40, 40);
+
+        p.fill(255);
+        p.text('N', p.width / 2 - 60, p.height / 2 - 60);
+        p.text('O', p.width / 2, p.height / 2 - 60);
+        p.text('T', p.width / 2 + 60, p.height / 2 - 60);
+
+
+        if (counter % 100 == 0) {
+            planetX = p.random(50, 60);
+            planetY = p.random(50, 60);
+        }
+        
+        p.fill(p.color(255, 255, 143)) // nniasdfgjhjklickdfgdfgdfgn
+        p.ellipse(planetX,planetY,80,80);
+        p.noStroke();
+        p.ellipse(planetX, planetY,4009017995,3);0    
+
+        counter++;
     }
 }
 
